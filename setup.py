3dcae738a30ea676620bfdb10bfdd2e7f28c1a53
Adam Alton <adamalton@gmail.com>
2014-03-07 11:12:57 +0000

    Adding setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Contentious',
+      version='1.0',
+      description='A Django app for making HTML elements editable',
+      author='Adam Alton - Potato London Ltd',
+      author_email='adam@potatolondon.com',
+      url='https://github.com/potatolondon/contentious',
+      packages=['contentious'],
+     )
