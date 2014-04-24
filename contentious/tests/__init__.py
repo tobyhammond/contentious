65f6f1e149638f07823b1090e8e9f40e7e7a869f
Adam Alton <adamalton@gmail.com>
2014-04-24 17:12:41 +0100

    Removing an import of a test file which no longer exists (it was probably working from the pyc file after I moved the file).

--- contentious/tests/__init__.py
+++ contentious/tests/__init__.py
@@ -1,6 +1,5 @@
 from .. contrib.basicedit.tests import APITest as EditAPITest
 from .. contrib.basictrans.tests import APITest as TransAPITest
-from ..contrib.common.tests import *
 
 from .templatetags import *
 from .utils import *
